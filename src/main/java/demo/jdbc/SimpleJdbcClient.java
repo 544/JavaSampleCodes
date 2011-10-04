e29ec9c9e73a420726b820299954a59143e67a9d
masato <masato.t@gmail.com>
2011-10-04 22:39:41 +0900

    単純なJDBCのサンプル

---
+++ src/main/java/demo/jdbc/SimpleJdbcClient.java
@@ -0,0 +1,44 @@
+package demo.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import utils.SimpleLogger;
+
+public class SimpleJdbcClient {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"context/jdbcContext.xml");
+
+		// dataSourceはSpringで作成。
+		DataSource source = (DataSource) ctx.getBean("dataSource");
+		Connection con = source.getConnection();
+		Statement stat = con.createStatement();
+		PreparedStatement ps = con.prepareStatement("SELECT * FROM TEST WHERE NAME = ?");
+
+		// バインドパラメータの設定
+		ps.setString(1, "huge");
+		
+		// クエリ実行
+		ResultSet resultset = ps.executeQuery();
+
+		while (resultset.next()) {
+			String a = resultset.getString(1);
+			String b = resultset.getString(2);
+
+			SimpleLogger.debug(a + ":" + b);
+		}
+
+		resultset.close();
+		stat.close();
+		con.close();
+
+	}
+}
